dd964a54dc21f81f42e01853c8f4e42e8ee97f64
Federico Di Pierro <nierro92@gmail.com>
2017-08-03 19:35:51 +0200

    Force MODULES_NUM in clight.c

--- src/clight.c
+++ src/clight.c
@@ -46,7 +46,7 @@ static void main_poll(void);
 /*
  * pointers to init modules functions;
  */
-static void (*const set_selfs[])(void) = {
+static void (*const set_selfs[MODULES_NUM])(void) = {
     set_brightness_self, set_location_self, set_upower_self, set_gamma_self,
     set_gamma_smooth_self, set_signal_self, set_bus_self, set_dimmer_self,
     set_dimmer_smooth_self, set_dpms_self, set_xorg_self, set_inhibit_self, set_userbus_self
