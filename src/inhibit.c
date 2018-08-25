8613ac8e9c260253e25ba39074d7d05abc4d1d66
Federico Di Pierro <nierro92@gmail.com>
2018-08-25 20:01:41 +0200

    Inhibit module does not depend upon xorg.

--- src/inhibit.c
+++ src/inhibit.c
@@ -9,7 +9,7 @@ static int inhibit_init(void);
 static int on_inhibit_change(sd_bus_message *m, void *userdata, sd_bus_error *ret_error);
 
 static sd_bus_slot *slot;
-static struct dependency dependencies[] = { {HARD, USERBUS}, {HARD, XORG} };
+static struct dependency dependencies[] = { {HARD, USERBUS} };
 static struct self_t self = {
     .name = "Inhibit",
     .idx = INHIBIT,
@@ -23,12 +23,12 @@ void set_inhibit_self(void) {
 
 static void init(void) {
     int r = inhibit_init();
-    /* In case of errors, upower_init returns -1 -> disable inhibit. */
+    /* In case of errors, inhibit_init returns -1 -> disable inhibit. */
     INIT_MOD(r == 0 ? DONT_POLL : DONT_POLL_W_ERR);
 }
 
 static int check(void) {
-    /* check initial inhibit state */
+    /* check initial inhibit state (if interface is found) */
     struct bus_args inhibit_args = {"org.freedesktop.PowerManagement.Inhibit", "/org/freedesktop/PowerManagement/Inhibit", "org.freedesktop.PowerManagement.Inhibit", "HasInhibit", USER};
     int r = call(&state.pm_inhibited, "b", &inhibit_args, NULL);
     if (r < 0) {
