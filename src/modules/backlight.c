3ae047e89a9f55a46cdcbe291340d5922b9e1e11
Federico Di Pierro <nierro92@gmail.com>
2021-11-07 19:04:05 +0100

    Dropped timeouts_in_ms conf option as it provided no real improvements.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- src/modules/backlight.c
+++ src/modules/backlight.c
@@ -43,7 +43,6 @@ static sd_bus_slot *sens_slot, *bl_slot, *if_a_slot, *if_r_slot;
 static char *backlight_interface; // main backlight interface used to only publish BL_UPD msgs for a single backlight sn
 static const sd_bus_vtable conf_bl_vtable[] = {
     SD_BUS_VTABLE_START(0),
-    SD_BUS_PROPERTY("TimeoutsInMs", "b", NULL, offsetof(bl_conf_t, timeouts_in_ms), SD_BUS_VTABLE_PROPERTY_CONST),
     SD_BUS_WRITABLE_PROPERTY("NoAutoCalib", "b", NULL, set_auto_calib, offsetof(bl_conf_t, no_auto_calib), 0),
     SD_BUS_WRITABLE_PROPERTY("InhibitOnLidClosed", "b", NULL, NULL, offsetof(bl_conf_t, pause_on_lid_closed), 0),
     SD_BUS_WRITABLE_PROPERTY("CaptureOnLidOpened", "b", NULL, NULL, offsetof(bl_conf_t, capture_on_lid_opened), 0),
@@ -462,14 +461,7 @@ static void do_capture(bool reset_timer, bool capture_only) {
     }
 
     if (reset_timer) {
-        if (conf.bl_conf.timeouts_in_ms) {
-            int ns = get_current_timeout() * 1000000;
-            int s = ns / BILLION;
-            ns = ns % BILLION;
-            set_timeout(s, ns, bl_fd, 0);
-        } else {
-            set_timeout(get_current_timeout(), 0, bl_fd, 0);
-        }
+        set_timeout(get_current_timeout(), 0, bl_fd, 0);
     }
 }
 
--- src/utils/log.c
+++ src/utils/log.c
@@ -49,7 +49,6 @@ static void log_bl_conf(bl_conf_t *bl_conf) {
     fprintf(log_file, "* Daily timeouts:\t\tAC %d\tBATT %d\n", bl_conf->timeout[ON_AC][DAY], bl_conf->timeout[ON_BATTERY][DAY]);
     fprintf(log_file, "* Nightly timeouts:\t\tAC %d\tBATT %d\n", bl_conf->timeout[ON_AC][NIGHT], bl_conf->timeout[ON_BATTERY][NIGHT]);
     fprintf(log_file, "* Event timeouts:\t\tAC %d\tBATT %d\n", bl_conf->timeout[ON_AC][SIZE_STATES], bl_conf->timeout[ON_BATTERY][SIZE_STATES]);
-    fprintf(log_file, "* Timeouts in ms:\t\t%s\n", bl_conf->timeouts_in_ms ? "Enabled" : "Disabled");
     fprintf(log_file, "* Shutter threshold:\t\t%.2lf\n", bl_conf->shutter_threshold);
     fprintf(log_file, "* Autocalibration:\t\t%s\n", bl_conf->no_auto_calib ? "Disabled" : "Enabled");
     fprintf(log_file, "* Pause on lid closed:\t\t%s\n", bl_conf->pause_on_lid_closed ? "Enabled" : "Disabled");
