30e1d32ca8bbf1e723e30e1d57a7b53757248a93
Federico Di Pierro <nierro92@gmail.com>
2023-06-08 20:07:31 +0200

    cleanup(Extra,src): split clight conf file in many submodules to improve readability.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- src/conf/config.h
+++ src/conf/config.h
@@ -2,7 +2,8 @@
 
 #include "commons.h"
 
-enum CONFIG { GLOBAL, LOCAL, CUSTOM };
+enum CONFIG { GLOBAL, OLD_GLOBAL, LOCAL, CUSTOM };
 
+void init_config_file(enum CONFIG file, char *filename);
 int read_config(enum CONFIG file, char *config_file);
 int store_config(enum CONFIG file);
